4271d9fd040dab95018d470385c22c8dd03753bf
Artem Tishchenko <82469944+artem20210413@users.noreply.github.com>
2023-02-08 00:15:49 +0200

    first commit

---
+++ MOYKZ_1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace MOYKZ_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxInput = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOutput = new System.Windows.Forms.PictureBox();
+            this.buttonUpload = new System.Windows.Forms.Button();
+            this.buttonGray = new System.Windows.Forms.Button();
+            this.buttonInversion = new System.Windows.Forms.Button();
+            this.sizeInput = new System.Windows.Forms.Label();
+            this.sizeOutput = new System.Windows.Forms.Label();
+            this.buttonBinarization = new System.Windows.Forms.Button();
+            this.buttonIncreaseSize = new System.Windows.Forms.Button();
+            this.buttonСontrasting = new System.Windows.Forms.Button();
+            this.buttonIntropolation = new System.Windows.Forms.Button();
+            this.buttonHistogram = new System.Windows.Forms.Button();
+            this.textBoxBinarization = new System.Windows.Forms.TextBox();
+            this.cumulativeHistogram = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxInput
+            // 
+            this.pictureBoxInput.Location = new System.Drawing.Point(12, 28);
+            this.pictureBoxInput.Name = "pictureBoxInput";
+            this.pictureBoxInput.Size = new System.Drawing.Size(795, 570);
+            this.pictureBoxInput.TabIndex = 0;
+            this.pictureBoxInput.TabStop = false;
+            // 
+            // pictureBoxOutput
+            // 
+            this.pictureBoxOutput.Location = new System.Drawing.Point(834, 28);
+            this.pictureBoxOutput.Name = "pictureBoxOutput";
+            this.pictureBoxOutput.Size = new System.Drawing.Size(795, 570);
+            this.pictureBoxOutput.TabIndex = 1;
+            this.pictureBoxOutput.TabStop = false;
+            // 
+            // buttonUpload
+            // 
+            this.buttonUpload.Location = new System.Drawing.Point(12, 604);
+            this.buttonUpload.Name = "buttonUpload";
+            this.buttonUpload.Size = new System.Drawing.Size(795, 201);
+            this.buttonUpload.TabIndex = 2;
+            this.buttonUpload.Text = "Upload picture";
+            this.buttonUpload.UseVisualStyleBackColor = true;
+            this.buttonUpload.Click += new System.EventHandler(this.buttonUpload_Click);
+            // 
+            // buttonGray
+            // 
+            this.buttonGray.Location = new System.Drawing.Point(834, 604);
+            this.buttonGray.Name = "buttonGray";
+            this.buttonGray.Size = new System.Drawing.Size(239, 52);
+            this.buttonGray.TabIndex = 3;
+            this.buttonGray.Text = "Convert to gray";
+            this.buttonGray.UseVisualStyleBackColor = true;
+            this.buttonGray.Click += new System.EventHandler(this.buttonGray_Click);
+            // 
+            // buttonInversion
+            // 
+            this.buttonInversion.Location = new System.Drawing.Point(834, 662);
+            this.buttonInversion.Name = "buttonInversion";
+            this.buttonInversion.Size = new System.Drawing.Size(239, 52);
+            this.buttonInversion.TabIndex = 5;
+            this.buttonInversion.Text = "Color inversion";
+            this.buttonInversion.UseVisualStyleBackColor = true;
+            this.buttonInversion.Click += new System.EventHandler(this.buttonInversion_Click);
+            // 
+            // sizeInput
+            // 
+            this.sizeInput.AutoSize = true;
+            this.sizeInput.Location = new System.Drawing.Point(12, 9);
+            this.sizeInput.Name = "sizeInput";
+            this.sizeInput.Size = new System.Drawing.Size(61, 16);
+            this.sizeInput.TabIndex = 6;
+            this.sizeInput.Text = "SizeInput";
+            // 
+            // sizeOutput
+            // 
+            this.sizeOutput.AutoSize = true;
+            this.sizeOutput.Location = new System.Drawing.Point(831, 9);
+            this.sizeOutput.Name = "sizeOutput";
+            this.sizeOutput.Size = new System.Drawing.Size(71, 16);
+            this.sizeOutput.TabIndex = 7;
+            this.sizeOutput.Text = "SizeOutput";
+            // 
+            // buttonBinarization
+            // 
+            this.buttonBinarization.Location = new System.Drawing.Point(1112, 604);
+            this.buttonBinarization.Name = "buttonBinarization";
+            this.buttonBinarization.Size = new System.Drawing.Size(239, 52);
+            this.buttonBinarization.TabIndex = 8;
+            this.buttonBinarization.Text = "Binarization";
+            this.buttonBinarization.UseVisualStyleBackColor = true;
+            this.buttonBinarization.Click += new System.EventHandler(this.buttonBinarization_Click);
+            // 
+            // buttonIncreaseSize
+            // 
+            this.buttonIncreaseSize.Location = new System.Drawing.Point(1390, 604);
+            this.buttonIncreaseSize.Name = "buttonIncreaseSize";
+            this.buttonIncreaseSize.Size = new System.Drawing.Size(239, 52);
+            this.buttonIncreaseSize.TabIndex = 10;
+            this.buttonIncreaseSize.Text = "Extropolation";
+            this.buttonIncreaseSize.UseVisualStyleBackColor = true;
+            this.buttonIncreaseSize.Click += new System.EventHandler(this.buttonIncreaseSize_Click);
+            // 
+            // buttonСontrasting
+            // 
+            this.buttonСontrasting.Location = new System.Drawing.Point(834, 720);
+            this.buttonСontrasting.Name = "buttonСontrasting";
+            this.buttonСontrasting.Size = new System.Drawing.Size(239, 52);
+            this.buttonСontrasting.TabIndex = 11;
+            this.buttonСontrasting.Text = "Сontrasting";
+            this.buttonСontrasting.UseVisualStyleBackColor = true;
+            this.buttonСontrasting.Click += new System.EventHandler(this.buttonСontrasting_Click);
+            // 
+            // buttonIntropolation
+            // 
+            this.buttonIntropolation.Location = new System.Drawing.Point(1390, 662);
+            this.buttonIntropolation.Name = "buttonIntropolation";
+            this.buttonIntropolation.Size = new System.Drawing.Size(239, 52);
+            this.buttonIntropolation.TabIndex = 12;
+            this.buttonIntropolation.Text = "Intropolation";
+            this.buttonIntropolation.UseVisualStyleBackColor = true;
+            this.buttonIntropolation.Click += new System.EventHandler(this.buttonIntropolation_Click);
+            // 
+            // buttonHistogram
+            // 
+            this.buttonHistogram.Location = new System.Drawing.Point(1112, 695);
+            this.buttonHistogram.Name = "buttonHistogram";
+            this.buttonHistogram.Size = new System.Drawing.Size(239, 52);
+            this.buttonHistogram.TabIndex = 13;
+            this.buttonHistogram.Text = "linear histogram";
+            this.buttonHistogram.UseVisualStyleBackColor = true;
+            this.buttonHistogram.Click += new System.EventHandler(this.buttonLinearHistogram_Click);
+            // 
+            // textBoxBinarization
+            // 
+            this.textBoxBinarization.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxBinarization.Location = new System.Drawing.Point(1112, 662);
+            this.textBoxBinarization.Name = "textBoxBinarization";
+            this.textBoxBinarization.Size = new System.Drawing.Size(239, 27);
+            this.textBoxBinarization.TabIndex = 9;
+            this.textBoxBinarization.Text = "127";
+            this.textBoxBinarization.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cumulativeHistogram
+            // 
+            this.cumulativeHistogram.Location = new System.Drawing.Point(1112, 753);
+            this.cumulativeHistogram.Name = "cumulativeHistogram";
+            this.cumulativeHistogram.Size = new System.Drawing.Size(239, 52);
+            this.cumulativeHistogram.TabIndex = 14;
+            this.cumulativeHistogram.Text = "cumulative histogram";
+            this.cumulativeHistogram.UseVisualStyleBackColor = true;
+            this.cumulativeHistogram.Click += new System.EventHandler(this.cumulativeHistogram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1641, 815);
+            this.Controls.Add(this.cumulativeHistogram);
+            this.Controls.Add(this.buttonСontrasting);
+            this.Controls.Add(this.buttonIncreaseSize);
+            this.Controls.Add(this.buttonBinarization);
+            this.Controls.Add(this.buttonGray);
+            this.Controls.Add(this.buttonHistogram);
+            this.Controls.Add(this.buttonIntropolation);
+            this.Controls.Add(this.textBoxBinarization);
+            this.Controls.Add(this.sizeOutput);
+            this.Controls.Add(this.sizeInput);
+            this.Controls.Add(this.buttonInversion);
+            this.Controls.Add(this.buttonUpload);
+            this.Controls.Add(this.pictureBoxOutput);
+            this.Controls.Add(this.pictureBoxInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxOutput;
+        private System.Windows.Forms.Button buttonUpload;
+        private System.Windows.Forms.Button buttonGray;
+        private System.Windows.Forms.Button buttonInversion;
+        private System.Windows.Forms.Label sizeInput;
+        private System.Windows.Forms.Label sizeOutput;
+        private System.Windows.Forms.Button buttonBinarization;
+        private System.Windows.Forms.Button buttonIncreaseSize;
+        private System.Windows.Forms.Button buttonСontrasting;
+        private System.Windows.Forms.Button buttonIntropolation;
+        private System.Windows.Forms.Button buttonHistogram;
+        private System.Windows.Forms.TextBox textBoxBinarization;
+        private System.Windows.Forms.Button cumulativeHistogram;
+    }
+}
+
---
+++ MOYKZ_1/Form1.cs
@@ -0,0 +1,429 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOYKZ_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void showSizeInput()
+        {
+            if (_imgInput != null)
+                sizeInput.Text = _imgInput.Size.Width.ToString() + "x" + _imgInput.Size.Height.ToString();
+           
+        }
+        private void showSizeOutput()
+        {
+            if (_imgOutput != null)
+                sizeOutput.Text = _imgOutput.Size.Width.ToString() + "x" + _imgOutput.Size.Height.ToString();
+
+        }
+
+        private Bitmap _imgInput;
+        private Bitmap _imgOutput;
+
+        private void buttonUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxInput.SizeMode = PictureBoxSizeMode.Zoom;
+                this._imgInput = new Bitmap(openFile.FileName);
+                pictureBoxInput.Image = this._imgInput;
+                showSizeInput();
+            }
+
+            
+        }
+
+        private void buttonGray_Click(object sender, EventArgs e)
+        {
+          //  Bitmap copyBitmap = new Bitmap(pictureBoxInput.Image.Width, pictureBoxInput.Image.Height);
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+            ProcessImage(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+
+        public void ProcessImage(Bitmap bmp)
+        {
+            for(int i = 0; i < bmp.Width; i++)
+            {
+                for(int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor =  bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    //int gray = (byte)(.299 * red + .587 * green + .114 * blue);
+                    int gray = (byte)((red+green+blue)/3);
+                    red = gray;
+                    green = gray;
+                    blue = gray;
+
+                    bmp.SetPixel(i,j,Color.FromArgb(red, green, blue));
+                }
+
+            }
+
+        }
+
+        private void buttonInversion_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+            colorInversion(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+
+        private void colorInversion(Bitmap bmp)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = 255 - bmpColor.R;
+                    int green = 255 - bmpColor.G;
+                    int blue = 255 - bmpColor.B;
+
+                    bmp.SetPixel(i, j, Color.FromArgb(red, green, blue));
+                }
+
+            }
+
+        }
+
+        private void buttonBinarization_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+            byte T = Convert.ToByte(textBoxBinarization.Text);
+
+            binarization(this._imgOutput, T);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+
+        private void binarization(Bitmap bmp, int T)
+        {
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int average = (byte)((red + green + blue) / 3);
+                    byte binarization;
+                    if (average < T)
+                    {
+                        binarization = 0;
+                    }
+                    else
+                    {
+                        binarization = 255;
+                    }
+
+                    bmp.SetPixel(i, j, Color.FromArgb(binarization, binarization, binarization));
+                }
+
+            }
+        }
+
+        
+
+        
+
+        private void buttonСontrasting_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+
+            contrasting(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+
+        private void contrasting(Bitmap bmp)
+        {
+            double min = 255, max = 0, color;
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+
+                    color = Convert.ToInt32((bmpColor.R + bmpColor.G + bmpColor.B) / 3);
+
+                    if (min > color)
+                        min = color;
+                    if (max < color)
+                        max = color;
+                }
+
+            }
+            int result;
+            int fix(double value)
+            {
+                result = Convert.ToInt32( Math.Round(((value - min) / (max - min)) * 255));
+                return (result < 0) ? 0 : (result > 255) ? 255 : result;
+            }
+            int red, green, blue;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+
+                    red = fix(bmpColor.R);
+                    green = fix(bmpColor.G);
+                    blue = fix(bmpColor.B);
+                    bmp.SetPixel(i, j, Color.FromArgb(bmpColor.A, red, green, blue));
+                }
+            }
+        }
+        private void buttonIncreaseSize_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+
+            extropolation(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+        private void extropolation(Bitmap bmp)
+        {
+            Bitmap bitmap = new Bitmap(bmp.Width*2, bmp.Height * 2, PixelFormat.Format24bppRgb);
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    bitmap.SetPixel(    i * 2,          j * 2       , bmp.GetPixel(i, j));
+                    bitmap.SetPixel(    i * 2 + 1,      j * 2       , bmp.GetPixel(i, j));
+                    bitmap.SetPixel(    i * 2,          j * 2 + 1   , bmp.GetPixel(i, j));
+                    bitmap.SetPixel(    i * 2 + 1,      j * 2 + 1   , bmp.GetPixel(i, j));
+
+                }
+            }
+            this._imgOutput = bitmap;
+
+        }
+        
+
+        private void buttonIntropolation_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+
+            intropolation(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxOutput.Image = this._imgOutput;
+
+            showSizeOutput();
+        }
+
+        private void intropolation(Bitmap bmp)
+        {
+
+            byte s(double a, double b)
+            { 
+                return Convert.ToByte(Math.Round((a + b) / 2));
+            }
+
+            Bitmap bitmap = new Bitmap(bmp.Width * 2, bmp.Height * 2, PixelFormat.Format24bppRgb);
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color C = bmp.GetPixel(i, j);              //bmpColor
+                    bitmap.SetPixel(i * 2, j * 2, bmp.GetPixel(i, j));
+
+                    if (i < bmp.Width-1)
+                    {
+                        Color CR = bmp.GetPixel(i + 1, j);     //bmpColorR
+                        bitmap.SetPixel(i * 2 + 1, j * 2, Color.FromArgb(s(C.A, CR.A), s(C.R, CR.R), s(C.G, CR.G), s(C.B, CR.B)));
+                    }
+                    else
+                        bitmap.SetPixel(i * 2 + 1, j * 2, bmp.GetPixel(i, j));
+
+                    if (j < bmp.Height-1)
+                    {
+                        Color CB = bmp.GetPixel(i, j + 1);     //bmpColorB
+                        bitmap.SetPixel(i * 2, j * 2 + 1, Color.FromArgb(s(C.A, CB.A), s(C.R, CB.R), s(C.G, CB.G), s(C.B, CB.B)));
+                    }
+                    else
+                        bitmap.SetPixel(i * 2, j * 2 + 1, bmp.GetPixel(i, j));
+
+                    if (i < bmp.Width-1 && j < bmp.Height-1)
+                    {
+                        Color CRB = bmp.GetPixel(i + 1, j + 1);  //bmpColorRB
+                        bitmap.SetPixel(i * 2 + 1, j * 2 + 1, Color.FromArgb(s(C.A, CRB.A), s(C.R, CRB.R), s(C.G, CRB.G), s(C.B, CRB.B)));
+                    }
+                    else
+                        bitmap.SetPixel(i * 2 + 1, j * 2 + 1, bmp.GetPixel(i, j));
+                }
+            }
+            this._imgOutput = bitmap;
+        }
+
+        private void buttonLinearHistogram_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+
+
+            this._imgOutput = linearHistogram(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxOutput.Image = this._imgOutput;
+
+
+        }
+
+        private void cumulativeHistogram_Click(object sender, EventArgs e)
+        {
+            this._imgOutput = new Bitmap((Bitmap)this._imgInput);
+
+            this._imgOutput = _cumulativeHistogram(this._imgOutput);
+            pictureBoxOutput.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxOutput.Image = this._imgOutput;
+
+        }
+        private Bitmap _cumulativeHistogram(Bitmap bmp)
+        {
+
+            int[] arrBright = new int[256];
+            
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int gray = (byte)((red + green + blue) / 3);
+
+                    arrBright[gray]++;
+
+                }
+
+            }
+
+            arrBright = cumulativeSort(arrBright);
+
+            return showHistogram(arrBright);
+        }
+
+        private Bitmap linearHistogram(Bitmap bmp)
+        {
+
+            int[] arrBright = new int[256];
+
+
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color bmpColor = bmp.GetPixel(i, j);
+                    int red = bmpColor.R;
+                    int green = bmpColor.G;
+                    int blue = bmpColor.B;
+                    int gray = (byte)((red + green + blue) / 3);
+
+                    arrBright[gray]++;
+
+                }
+
+            }
+
+            return showHistogram(arrBright);
+        }
+
+        int[] bubbleSort(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                for (int j = 0; j < A.Length - 1; j++)
+                {
+                    if (A[j] > A[j + 1])
+                    {
+                        int z = A[j];
+                        A[j] = A[j + 1];
+                        A[j + 1] = z;
+                    }
+                }
+            }
+            return A;
+        }
+        int[] cumulativeSort(int[] A)
+        {
+            for (int i = 1; i < A.Length; i++)
+            {
+               //A[i] += (i!=0)? A[i - 1]: A[i];
+               A[i] += A[i - 1];
+            }
+            return A;
+        }
+
+
+
+
+        private Bitmap showHistogram(int[] arrBright)
+        {
+
+            int maxCountBright = 0;
+            foreach (int el in arrBright)
+            {
+                if (maxCountBright < el)
+                    maxCountBright = el;
+
+            }
+
+            int x = 255, y = maxCountBright;
+
+            Bitmap bitmap = new Bitmap(x, y, PixelFormat.Format24bppRgb);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    if (j <= arrBright[i])
+                        bitmap.SetPixel(i, y - j - 1, Color.FromArgb(0, 0, 0, 0));
+                    else
+                        bitmap.SetPixel(i, y - j - 1, Color.FromArgb(255, 255, 255, 255));
+
+                }
+            }
+            return bitmap;
+
+        }
+
+       
+    }
+}
